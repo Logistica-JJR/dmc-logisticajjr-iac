1876a5a00215d86683555cd668df5983397f9ba2
unknown <ricvera@favoritafc.com>
2026-03-08 09:31:33 -0500

    Uso de RG existente a nivel de Terraform

--- main.tf
+++ main.tf
@@ -15,9 +15,8 @@ provider "azurerm" {
 # -------------------------------
 # Resource Group
 # -------------------------------
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = var.rg_name
 }
 
 # -------------------------------
@@ -26,13 +25,13 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = var.backend_subnet
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
@@ -50,7 +49,7 @@ resource "azurerm_subnet" "backend" {
 
 resource "azurerm_subnet" "frontend" {
   name                 = var.frontend_subnet
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 
@@ -71,7 +70,7 @@ resource "azurerm_subnet" "frontend" {
 # -------------------------------
 resource "azurerm_container_app_environment" "cae" {
   name                = var.cae_name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
@@ -80,7 +79,7 @@ resource "azurerm_container_app_environment" "cae" {
 # -------------------------------
 resource "azurerm_container_app" "backend" {
   name                         = var.backend_ca
-  resource_group_name          = azurerm_resource_group.rg.name
+  resource_group_name          = data.azurerm_resource_group.rg.name
   container_app_environment_id = azurerm_container_app_environment.cae.id
   revision_mode                = "Single"
 
@@ -109,7 +108,7 @@ resource "azurerm_container_app" "backend" {
 # -------------------------------
 resource "azurerm_container_app" "frontend" {
   name                         = var.frontend_ca
-  resource_group_name          = azurerm_resource_group.rg.name
+  resource_group_name          = data.azurerm_resource_group.rg.name
   container_app_environment_id = azurerm_container_app_environment.cae.id
   revision_mode                = "Single"
 
