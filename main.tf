91c8c13f0aae4b554cfad1719e42e6ea1ba16db5
unknown <ricvera@favoritafc.com>
2026-03-09 01:02:20 -0500

    Backend sobre HTTPS

--- main.tf
+++ main.tf
@@ -102,6 +102,7 @@ resource "azurerm_container_app" "backend" {
   ingress {
     external_enabled = false
     target_port      = 5000
+	transport        = "https"
 
     traffic_weight {
       latest_revision = true
