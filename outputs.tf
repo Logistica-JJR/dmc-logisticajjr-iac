dfd305f3dd388feff560a34e44e1f7a4f2cc888d
unknown <ricvera@favoritafc.com>
2026-03-09 00:13:46 -0500

    Mostrar URL interna de backend en AZ generada

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,11 @@ output "frontend_container_app_name" {
   value = azurerm_container_app.frontend.name
 }
 
+output "backend_fqdn" {
+  value       = azurerm_container_app.backend.latest_revision_fqdn
+  description = "URL interna del backend"
+}
+
 output "frontend_fqdn" {
   value       = azurerm_container_app.frontend.latest_revision_fqdn
   description = "URL pública del frontend"
